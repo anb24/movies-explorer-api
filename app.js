154cc175cbc5d4df85b9982e845e388f73a392aa
AndreyB <AnB.124@yandex.ru>
2022-06-04 17:54:52 +0700

    Update app.js

--- app.js
+++ app.js
@@ -13,21 +13,26 @@ const router = require('./routes/index');
 const { PORT = 3000, MONGO_URL = DEV_DATABASE_URL } = process.env;
 const app = express();
 
-mongoose.connect(MONGO_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+app.use('*', cors());
+// app.use(cors({
+//   origin: ['https://diplomar.nomoredomains.xyz', 'http://diplomar.nomoredomains.xyz', 'https://localhost:3000', 'http://localhost:3000', 'https://localhost:3001', 'http://localhost:3001'],
+//   credentials: true,
+// }));
 
 app.use(helmet());
 app.use(express.json());
 app.use(requestLogger);
-app.use(cors());
 app.use(limiter);
 app.use(router); // роуты
 
 app.use(errorLogger);
 app.use(errors());
 app.use(errorHandler);
+
+mongoose.connect(MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.listen(PORT, () => {
   console.log(`Запуск сервера на порту: ${PORT}`);
